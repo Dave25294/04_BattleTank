e91289f177c1f799cb331748c22289bffdf0623a
REVOLUTION\David <cess252@gmail.com>
2016-12-08 10:31:39 +0000

    BT67 Constructor & Begin Play Timing

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -14,6 +14,7 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	UE_LOG(LogTemp, Warning, TEXT("Constructor called"))
 }
 
 void ATank::AimAt(FVector HitLocation)
