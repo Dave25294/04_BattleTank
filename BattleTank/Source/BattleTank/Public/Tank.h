539096b8c03ea939415ba6d1a293b3c3bad0e299
REVOLUTION\David <cess252@gmail.com>
2016-12-09 16:39:53 +0000

    BT75 Finishing our Refactoring

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -5,38 +5,12 @@
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h" // Put new includes above
 
-// Forward declarations
-class UTankBarrel;
-class UTankTurret;
-class UTankTrack;
-class AProjectile;
-
 UCLASS()
 class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-public:
-	UFUNCTION(BlueprintCallable, Category = Firing)
-		void Fire();
-
 private:
 	// Sets default values for this pawn's properties
 	ATank();
-
-	virtual void BeginPlay() override;
-	
-	// TODO remove once firing is moved to aiming component
-	UPROPERTY(EditDefaultsOnly, Category = Firing)
-		float LaunchSpeed = 4000; 
-
-	UPROPERTY(EditDefaultsOnly, Category = Setup)
-		TSubclassOf<AProjectile> ProjectileBlueprint;
-
-	UTankBarrel* Barrel = nullptr; // TODO remove
-
-	UPROPERTY(EditDefaultsOnly, Category = Firing)
-		float ReloadTimeInSeconds = 3;
-
-	double LastFireTime = 0;
 };
